db4d161cca4f927e2badfcb0818256ebe59b226d
haoxinsun <haoxinsun@gmail.com>
2018-11-17 22:05:48 +0800

    Hello_1.py modify first by Student A

--- Hello_1.py
+++ Hello_1.py
@@ -2,3 +2,4 @@ if __name__=="__main__":
 	print("You are in the file Hello_1.py")
 	print("Student A modify the Hello_1.py file")
 	print("Student A modify the Hello_1.py file again")
+	print("Student A modify the file first")
