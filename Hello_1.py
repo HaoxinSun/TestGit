c1f9872e277dccf26d4b3edd087293070ba41e2e
haoxinsun <haoxinsun@gmail.com>
2018-11-18 10:34:20 +0800

    modify both two files

--- Hello_1.py
+++ Hello_1.py
@@ -4,3 +4,4 @@ if __name__=="__main__":
 =======
 	print("Student A modify Hello_1.py first")
 >>>>>>> 7abea2e32481481ec978faf97d6604659fed3d83
+        print("Student A modify Hello_1.py first")
--- Hello_2.py
+++ Hello_2.py
@@ -1,3 +1,4 @@
 if __name__=="__main__":
 	print("You are int he file Hello_2.py")
 	print("Student B modify the Hello_2.py file")
+        print("Student A modify the Hello_2.py first")
